96890fb4c0d06b02e1dbe16be675d68d34452846
Andreas Hartel <andreas.hartel@kip.uni-heidelberg.de>
2012-12-07 15:02:50 +0100

    calibre_pex now working as intended

--- source/waf/calibre_pex.py
+++ source/waf/calibre_pex.py
@@ -19,7 +19,7 @@ def configure(conf):
 			'-64',
 		]
 		conf.env['CALIBRE_PEX_OPT_LVS'] = [
-			'-64', '-spice', '-turbo',
+			'-64', '-turbo',
 		]
 
 
@@ -29,11 +29,14 @@ def create_calibre_pex_task(self):
 
 	self.rule_file = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),'calibre_pex_rules_'+self.cellname))
 
+	self.output_file_base = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),self.env.BRICK_RESULTS,self.cellname))
+	self.svdb = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),self.env.BRICK_RESULTS,'svdb'))
+
 	f = open(self.rule_file.abspath(),"w")
 	try:
 		f.write("""
 LAYOUT PATH "{0}"
-LAYOUT PRIMARY {1}
+LAYOUT PRIMARY "{1}"
 LAYOUT SYSTEM GDSII
 LAYOUT CASE YES
 
@@ -42,10 +45,13 @@ SOURCE PRIMARY "{3}"
 SOURCE SYSTEM SPICE
 SOURCE CASE YES
 
-MASK SVDB DIRECTORY "svdb" QUERY XRC CCI NOPINLOC IXF NXF SLPH
+MASK SVDB DIRECTORY "{5}" QUERY XRC CCI NOPINLOC IXF NXF SLPH
 
-PEX NETLIST "{3}.pex.netlist" HSPICE 1 SOURCENAMES GROUND "gnd" LOCATION RLOCATION RWIDTH RLENGTH RLAYER RTHICKNESS 
-PEX REPORT "{3}.pex.report" SOURCENAMES
+LVS REPORT "{4}.lvs.report"
+
+PEX NETLIST "{4}.pex.netlist" HSPICE 1 SOURCENAMES GROUND "gnd" LOCATION RCNAMED RLOCATION RWIDTH RLENGTH RLAYER RTHICKNESS 
+PEX REPORT "{4}.pex.report" SOURCENAMES
+PEX EXTRACT INCLUDE SOURCENAMES TOPLEVEL "{6}"
 PEX REDUCE ANALOG NO
 PEX REDUCE MINCAP COMBINE 0.001
 PEX REDUCE MINCAP REMOVE 0
@@ -56,18 +62,29 @@ PEX NETLIST VIRTUAL CONNECT NO
 PEX NETLIST NOXREF NET NAMES NO
 PEX NETLIST MUTUAL RESISTANCE YES
 
-LVS REPORT "{4}.lvs.report"
 
-LVS REPORT OPTION A AV B BV C CV D E E1 F FX G H I N O P R RA S V W X
-LVS REPORT MAXIMUM ALL
+LVS REPORT OPTION NONE
+LVS FILTER UNUSED OPTION AG RC RE RG SOURCE
+LVS FILTER UNUSED OPTION AG RC RE RG LAYOUT
 
 LVS RECOGNIZE GATES NONE
 
 DRC ICSTATION YES
 
-LVS POWER  NAME VDD
-LVS GROUND NAME GND
-		""".format(self.layout_gds.abspath(),self.cellname,self.source_netlist.abspath(),self.cellname,self.cellname))
+LVS POWER NAME
+"vdd"
+"vdd12a"
+"vdd12d"
+"vdd25a"
+"vdd25d"
+"vdd"
+LVS GROUND NAME
+"gnd"
+"gnda"
+"gndd"
+"gnd"
+		""".format(self.layout_gds.abspath(),self.cellname,self.source_netlist.abspath(),self.cellname,self.output_file_base.abspath(),self.svdb.abspath(),'" "'.join(getattr(self,'only_extract_nets',[]))))
+
 	except AttributeError as e:
 		error_string = str(e)
 		error_string = error_string.replace('\'task_gen\' object has no','')
@@ -75,22 +92,27 @@ LVS GROUND NAME GND
 		return
 
 	for inc in self.includes:
-		f.write('\nINCLUDE '+inc.abspath())
+		f.write('\nINCLUDE "'+inc.abspath()+'"')
 
-	for line in self.mixins:
-		f.write('\n'+line)
+	#for line in getattr(self,'mixins',[]):
+	#	f.write('\n'+line)
 
 	f.close()
 
-	t = self.create_task('calibrePexTask', [self.layout_gds,self.source_netlist], self.path.make_node(self.cellname+".pex.report"))
+	t = self.create_task('calibrePexTask', [self.layout_gds,self.source_netlist], self.output_file_base.change_ext(".pex.netlist"))
 
-@Task.always_run
+
 class calibrePexTask(Task.Task):
-	vars = ['CALIBRE_PEX','CALIBRE_PEX_OPT_PHDB','CALIBRE_PEX_OPT_PDB','CALIBRE_PEX_OPT_FMT']
+	vars = ['CALIBRE_PEX','CALIBRE_PEX_OPT_LVS','CALIBRE_PEX_OPT_PDB','CALIBRE_PEX_OPT_FMT']
 
 	def run_pex(self,stepname):
-		run_str = '%s -xrc -%s %s %s' % (self.env.CALIBRE_PEX, stepname, " ".join(self.env['CALIBRE_PEX_OPT_'+stepname.upper()]), self.generator.rule_file.abspath())
+		conditional_options = ""
+		if hasattr(self.generator,'only_extract_nets') and stepname == 'pdb':
+			conditional_options += ' -select'
+		run_str = '%s -xrc -%s %s %s %s' % (self.env.CALIBRE_PEX, stepname, conditional_options," ".join(self.env['CALIBRE_PEX_OPT_'+stepname.upper()]), self.generator.rule_file.abspath())
+
 		out = ""
+
 		try:
 			out = self.generator.bld.cmd_and_log(run_str)
 		except Exception as e:
@@ -102,7 +124,7 @@ class calibrePexTask(Task.Task):
 		f.close()
 
 	def run_lvs(self):
-		run_str = '%s -lvs -hier %s %s' % (self.env.CALIBRE_PEX, " ".join(self.env['CALIBRE_PEX_OPT_LVS']), self.generator.rule_file.abspath())
+		run_str = '%s -lvs -hier -spice %s %s %s' % (self.env.CALIBRE_PEX, self.generator.svdb.make_node(self.generator.cellname+'.sp').abspath(), " ".join(self.env['CALIBRE_PEX_OPT_LVS']), self.generator.rule_file.abspath())
 		out = ""
 		try:
 			out = self.generator.bld.cmd_and_log(run_str)
