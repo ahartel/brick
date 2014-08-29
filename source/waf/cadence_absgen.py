b2f6a3010a29e64c2d5ae2abd4e02393dcc2276f
Andreas Hartel <andreas.hartel@kip.uni-heidelberg.de>
2014-08-29 11:46:32 +0200

    cadence_absgen: removed uneccessary code, added taskgen functions for result and logfile node, added option dont_export_lef_file

--- source/waf/cadence_absgen.py
+++ source/waf/cadence_absgen.py
@@ -1,4 +1,5 @@
 import os,re
+from brick_general import ChattyBrickTask
 from waflib import Task,Errors,Node,TaskGen,Configure,Context,Logs
 
 def configure(conf):
@@ -73,8 +74,8 @@ absSelectBin("Block")
 
 	f.write("""
 ;       absSetBinOption("Block" "PinsBoundaryCreate" "as needed")
-;absSetBinOption("Block" "PinsBoundaryCreate" "always")
-;absSetBinOption("Block" "PinsBoundaryLayers" "prBoundary")
+absSetBinOption("Block" "PinsBoundaryCreate" "always")
+absSetBinOption("Block" "PinsBoundaryLayers" "M1 M2 M3 M4 M5 M6 M7 M8 M9 prBoundary")
 
 absSetBinOption("Block" "ExtractSig" "false")
 absSetBinOption("Block" "ExtractPwr" "false")
@@ -120,14 +121,14 @@ absSetBinOption("Block" "AbstractSiteNameDefine" "")
 			for pin, props in opts.iteritems():
 				f.write('absSetTerminalProp( "'+cell+'" "'+pin+'" "'+('" "'.join(props))+'")\n')
 
-	if export_lef_file:
+	if not getattr(self,'dont_export_lef_file',False):
 		f.write("""
 absSetOption("ExportLEFVersion" 	"5.5")
 absSetOption("ExportGeometryLefData" 	"true")
 absSetOption("ExportTechLefData" 	"false")
 absSetOption("ExportLEFBin" 		"Block")
 absSetOption("ExportLEFFile" 		"{0}")
-""".format(export_lef_file.abspath()))
+""".format(self.get_cadence_absgen_result_node().abspath()))
 
 	f.write("""
 absPins()
@@ -135,9 +136,9 @@ absExtract()
 absAbstract()
 
 absSetCellProp("{0}" "symmetry" "X Y R90")
-""".format(self.cellname))
+""".format(cellname))
 
-	if export_lef_file:
+	if not getattr(self,'dont_export_lef_file',False):
 		f.write("""
 absExportLEF()
 """)
@@ -150,162 +151,39 @@ absExit()
 
 	inputs = [input_layout_node]
 	outputs = []
-	if export_lef_file:
-		outputs.append(export_lef_file)
+	if hasattr(self,'export_lef_file'):
+		outputs.append(self.export_lef_file)
+	else:
+		outputs.append(self.get_cadence_absgen_result_node())
 
 	t = self.create_task('cdsAbsgenTask',inputs,outputs)
 
-class cdsAbsgenTask(Task.Task):
+
+@TaskGen.taskgen_method
+def get_cadence_absgen_logfile_node(self):
+	libname,cellname,viewname = self.get_cadence_lib_cell_view_from_cellview()
+	return self.get_logdir_node().make_node('abstract_'+libname+'_'+cellname+'.log')
+
+@TaskGen.taskgen_method
+def get_cadence_absgen_result_node(self):
+	if hasattr(self,'export_lef_file'):
+		return self.export_lef_file
+	else:
+		results_dir = self.bld.bldnode.find_node('results')
+		lib,cell,view = self.get_cadence_lib_cell_view_from_cellview()
+		return results_dir.make_node(lib+'_'+cell+'.lef')
+
+class cdsAbsgenTask(ChattyBrickTask):
 	vars = ['CADENCE_ABSTRACT','CADENCE_ABSTRACT_OPTIONS']
-
-	def run(self):
-		run_str = 'cd %s && %s %s -replay %s 2>&1' % (self.generator.path.get_bld().abspath(), self.env.CADENCE_ABSTRACT, " ".join(self.env.CADENCE_ABSTRACT_OPTIONS), self.generator.absgen_script.abspath())
-		out = ""
-		try:
-			out = self.generator.bld.cmd_and_log(run_str)#, quiet=Context.STDOUT)
-		except Exception as e:
-			out = e.stdout
-
-		logfile = self.generator.path.get_bld().make_node(os.path.join(self.generator.path.bld_dir(),self.env.BRICK_LOGFILES,'abstract_'+self.generator.libname+'_'+self.generator.cellname+'.log'))
-		f = open(logfile.abspath(),'w')
-		f.write(out)
-		f.close()
-
-		return 0
+	run_str = '${env.CADENCE_ABSTRACT} ${env.CADENCE_ABSTRACT_OPTIONS} -replay ${gen.absgen_script.abspath()} -log ${gen.get_cadence_absgen_logfile_node().abspath()}'
 
 
+	def check_output(self,ret,out):
+		if '*Error*' in out or 'ERROR' in out:
+			return 1
+		else:
+			return ret
 
 
-@TaskGen.feature('cadence_absgen_blabb')
-def blubb(self):
-
-	self.liberate_script = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),'liberate_script_'+self.cell+'.tcl'))
-
-	if not getattr(self,'netlists_spice',None):
-		self.netlists_spice = []
-	if not getattr(self,'netlists_spectre',None):
-		self.netlists_spectre = []
-
-	netlists_strings_spice = []
-	for netlist in getattr(self,'netlists_spice',[]):
-		#if not netlist:
-		#	Logs.error('You have given an undefined node object as netlist for feature "altos_liberate".')
-		#	return
-		try:
-			netlists_strings_spice.append(netlist.abspath())
-		except AttributeError:
-			netlists_strings_spice.append(netlist)
-
-	netlists_strings_spectre = []
-	for netlist in getattr(self,'netlists_spectre',[]):
-		#if not netlist:
-		#	Logs.error('You have given an undefined node object as netlist for feature "altos_liberate".')
-		#	return
-		try:
-			netlists_strings_spectre.append(netlist.abspath())
-		except AttributeError:
-			netlists_strings_spectre.append(netlist)
-
-
-	output_library = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),self.env.BRICK_RESULTS,self.cell+'.lib'))
-
-	f = open(self.liberate_script.abspath(),"w")
-	try:
-		tcl_string = """
-# Define templates for characterization.
-# Delay template for 3 input slews and 3 loads
-define_template -type constraint \\
-    -index_1 {{0.0001 0.0002 0.0003}} \\
-    -index_2 {{0.0001 0.0002 0.0003}} \\
-    constraint_3x3
-
-define_template -type delay \\
-    -index_1 {{0.026973 0.047707 0.103269 0.334201 0.800188 1.965635}} \\
-    -index_2 {{0.002000 0.006100 0.018607 0.056753 0.173106 0.528000}} \\
-    delay_6x6
-
-define_template -type delay \\
-    -index_1 {{0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0007}} \\
-    -index_2 {{0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0007}} \\
-    delay_7x7_0
-
-# Power template for 3 input slews and 3 loads
-define_template -type power \\
-    -index_1 {{0.026968 0.047707 0.103268 0.334201 0.800117 1.965723}} \\
-    -index_2 {{0.002000 0.006100 0.018607 0.056753 0.173106 0.528000}} \\
-    power_6x6
-
-define_template -type power \\
-    -index_1 {{0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0007}} \\
-    -index_2 {{0.0001 0.0002 0.0003 0.0004 0.0005 0.0006 0.0007}} \\
-    power_7x7_0
-
-# Specify the PVT for this characterization run
-set_operating_condition -voltage 1.2 -temp 25
-
-#set_var measure_slew_lower_fall 0.1
-#set_var measure_slew_upper_fall 0.9
-#set_var measure_slew_lower_rise 0.1
-#set_var measure_slew_upper_rise 0.9
-#
-#set_units -leakage_power 1pw
-""".format()
-		if netlists_strings_spice:
-			tcl_string += """
-read_spice -format hspice {{ \\
-	{0} \\
-}}
-""".format(' \\\n\t'.join(netlists_strings_spice))
-		if netlists_strings_spectre:
-			tcl_string += """
-read_spice -format spectre {{ \\
-	{0} \\
-}}
-""".format(' \\\n\t'.join(netlists_strings_spectre))
-		tcl_string += """
-set_vdd vdd 1.2
-set_vdd vdd12a 1.2
-set_vdd vdd12d 1.2
-set_gnd gnd 0
-set_gnd gndd 0
-set_gnd gnda 0
-
-define_cell \\
-    -input {{ {0} }} \\
-    -output {{ {1} }} \\
-    -clock {{ {2} }} \\
-    -bidi {{ {3} }} \\
-    -delay delay_7x7_0 \\
-    -power power_7x7_0 \\
-    -constraint constraint_3x3 \\
-	-pinlist {{ {0} {1} {2} {3} }} \\
-    {{ {4} }}
-
-""".format(
-				' '.join(getattr(self,'input',[])),
-				" ".join(getattr(self,'output',[])),
-				" ".join(getattr(self,'clock',[])),
-				" ".join(getattr(self,'bidi',[])),
-				self.cell,
-			)
-	except AttributeError:
-		Logs.error('Please define a cell name with parameter "cell" for feature "altos_liberate".')
-
-	if hasattr(self,'arcs'):
-		tcl_string += " \n".join(self.arcs)
-		tcl_string += "\n"
-
-	tcl_string += "char_library -skip {leakage power mpw delay}"
-	if hasattr(self,'io_only') and getattr(self,'io_only',False) == True:
-		tcl_string += ' -io'
-	if hasattr(self,'user_arcs_only') and getattr(self,'user_arcs_only',False) == True:
-		tcl_string += ' -user_arcs_only'
-
-	tcl_string += "\nwrite_library -overwrite {0}\n".format(output_library.abspath())
-
-	f.write(tcl_string)
-	f.close()
-
-	t = self.create_task('altosLibTask', self.netlists_spice+self.netlists_spectre, output_library)
 
 # vim: noexpandtab:
