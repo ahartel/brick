54b436443d432432e689968327fae6dcf1adb329
Andreas Hartel <andreas.hartel@kip.uni-heidelberg.de>
2014-08-29 17:30:45 +0200

    cleaned up cadence_strmout.py

--- source/waf/cadence_strmout.py
+++ source/waf/cadence_strmout.py
@@ -2,12 +2,13 @@ import os,re
 from waflib import Task,Errors,Node,TaskGen,Configure,Node,Logs
 
 def configure(conf):
-	conf.load('brick_general')
+	conf.load('cadence_base')
 
 	"""This function gets called by waf upon loading of this module in a configure method"""
 	if not conf.env.BRICK_LOGFILES:
 		conf.env.BRICK_LOGFILES = './logfiles'
-	conf.env['CDS_STRMOUT'] = 'strmout'
+
+	conf.find_program('strmout', var='CDS_STRMOUT')
 	conf.env['CDS_STRMOUT_OPTIONS'] = [
 			'-snapToGrid',
 			'-case', 'preserve'
@@ -16,41 +17,36 @@ def configure(conf):
 @TaskGen.feature('cds_strmout')
 def create_cds_strmout_task(self):
 	try:
-		cellview = getattr(self,'cellview','')
-		if cellview.find('.') == -1 or cellview.find(':') == -1:
-			Logs.error('Please specify a cellview of the form Lib:Cell:View with the \'view\' attribute with the feature \'cds_strmout\'.')
-			return
-		(self.libname,rest) = cellview.split(".")
-		(self.cellname,self.viewname) = rest.split(":")
-		try:
-			layout_node = self.get_cellview_path(cellview,True).make_node('layout.oa')
-		except AttributeError:
-			Logs.error("Cellview '"+cellview+"' not found in path "+self.get_cellview_path(cellview))
-			return
-
-		results_dir = self.path.get_bld().make_node('results')
-		if not os.path.exists(results_dir.abspath()):
-			results_dir.mkdir()
-
-		strmfile = getattr(self,'strmfile', results_dir.make_node(self.libname+'_'+self.cellname+'.gds'))
-
-		t = self.create_task('cdsStrmoutTask', layout_node, strmfile)
+		layout_node = self.get_cellview_path(self.cellview,True).make_node('layout.oa')
+	except AttributeError:
+		Logs.error("Cellview '"+cellview+"' not found in path "+self.get_cellview_path(cellview))
+		return 1
 	except ValueError:
-		raise Errors.ConfigurationError('For feature "cds_strmout", you need to specify a parameter "view" in the form of lib.cell:view')
+		Logs.Error('For feature "cds_strmout", you need to specify a parameter "cellview" in the form of lib.cell:view')
+		return 1
 
+	t = self.create_task('cdsStrmoutTask', layout_node, self.get_cadence_strmout_gds_node())
+
+@TaskGen.taskgen_method
+def get_cadence_strmout_gds_node(self):
+	lib,cell,view = self.get_cadence_lib_cell_view_from_cellview()
+	return getattr(self,'strmfile', self.get_cadence_strmout_results_dir().make_node(lib+'_'+cell+'.gds'))
+
+@TaskGen.taskgen_method
+def get_cadence_strmout_results_dir(self):
+	return self.bld.bldnode.find_node('results')
 
 class cdsStrmoutTask(Task.Task):
 	vars = ['CDS_STRMOUT']
 
 	def run(self):
 		"""Checking logfile for critical warnings line by line"""
+		libname, cellname, viewname = self.generator.get_cadence_lib_cell_view_from_cellview()
 
-		run_str = '${CDS_STRMOUT} -library '+self.generator.libname+' -topcell '+self.generator.cellname+' -view '+self.generator.viewname+' ${CDS_STRMOUT_OPTIONS} -strmfile ${TGT[0].abspath()}'
+		run_str = '${CDS_STRMOUT} -library '+libname+' -topcell '+cellname+' -view '+viewname+' ${CDS_STRMOUT_OPTIONS} -strmfile ${TGT[0].abspath()}'
 
 		(f, dvars) = Task.compile_fun(run_str, False)
 		return f(self)
-
-
 
 # for convenience
 @Configure.conf
