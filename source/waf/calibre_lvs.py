9a8fe3aa51a6b016fec94c311b5ae6782ab5647f
Andreas Hartel <andreas.hartel@kip.uni-heidelberg.de>
2014-10-21 13:36:52 +0200

    updated calibre_lvs tool to use ChattyBrickTask

--- source/waf/calibre_lvs.py
+++ source/waf/calibre_lvs.py
@@ -1,5 +1,6 @@
 import os,re
 from waflib import Task,Errors,Node,TaskGen,Configure,Node,Logs,Context
+from brick_general import ChattyBrickTask
 
 def configure(conf):
 	conf.load('brick_general')
@@ -16,10 +17,10 @@ def configure(conf):
 @TaskGen.feature('calibre_lvs')
 def create_calibre_lvs_task(self):
 
-	self.rule_file = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),'calibre_lvs_rules_'+self.cellname))
-	self.hcells_file =  self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),'calibre_hcells_'+self.cellname))
+	self.rule_file = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),'calibre_lvs_rules_'+self.layout_cellname))
+	self.hcells_file =  self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),'calibre_hcells_'+self.layout_cellname))
 
-	self.output_file_base = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),self.env.BRICK_RESULTS,self.cellname))
+	self.output_file_base = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),self.env.BRICK_RESULTS,self.layout_cellname))
 	self.svdb = self.path.get_bld().make_node(os.path.join(self.path.bld_dir(),self.env.BRICK_RESULTS,'svdb'))
 	if not os.path.exists(self.svdb.abspath()):
 		self.svdb.mkdir()
@@ -57,10 +58,8 @@ LVS FILTER UNUSED OPTION NONE LAYOUT
 
 LVS RECOGNIZE GATES {6}
 LVS ABORT ON SUPPLY ERROR YES
-LVS ISOLATE SHORTS NO
 LVS IGNORE PORTS NO
 LVS GLOBALS ARE PORTS NO
-VIRTUAL CONNECT COLON YES
 VIRTUAL CONNECT REPORT NO
 LVS EXECUTE ERC YES
 ERC RESULTS DATABASE "sram_row_pst.erc.results"
@@ -83,7 +82,7 @@ LVS GROUND NAME
 "gndd"
 "gnd"
 
-""".format(self.layout_gds.abspath(),self.cellname,self.source_netlist.abspath(),self.cellname,self.output_file_base.abspath(),self.svdb.abspath(),recognize_gates))
+""".format(self.layout_gds.abspath(),self.layout_cellname,self.source_netlist.abspath(),self.source_cellname,self.output_file_base.abspath(),self.svdb.abspath(),recognize_gates))
 
 	for line in getattr(self,'mixins',[]):
 		f.write(line+'\n')
@@ -98,43 +97,45 @@ LVS GROUND NAME
 		f.write("\n".join(getattr(self,'hcells',[])))
 		f.close()
 
-	output = self.svdb.make_node(self.cellname+'.sp')
+	output = self.svdb.make_node(self.layout_cellname+'.sp')
 	open(output.abspath(),'w').close() 
 
 	t = self.create_task('calibreLvsTask', [self.layout_gds,self.source_netlist], [self.output_file_base.change_ext(".lvs.report"), output])
 
-class calibreLvsTask(Task.Task):
+@TaskGen.taskgen_method
+def get_calibre_lvs_logfile_node(self):
+	return self.bld.bldnode.find_node(self.env.BRICK_LOGFILES).make_node('calibre_lvs_'+self.layout_cellname+'.log')
+
+@TaskGen.taskgen_method
+def get_calibre_lvs_spice_node(self):
+	return self.svdb.make_node(self.layout_cellname+'.sp')
+
+@TaskGen.taskgen_method
+def get_calibre_lvs_options(self):
+	conditional_options = ""
+	if hasattr(self,'hcells'):
+		conditional_options += ' -hcell '+self.hcells_file.abspath()
+	return conditional_options
+
+#@Task.always_run
+class calibreLvsTask(ChattyBrickTask):
 	vars = ['CALIBRE_LVS','CALIBRE_LVS_OPTIONS','CALIBRE_LVS_RULES']
-	def run(self):
-		conditional_options = ""
-		if hasattr(self.generator,'hcells'):
-			conditional_options += ' -hcell '+self.generator.hcells_file.abspath()
+	run_str = '${CALIBRE_LVS} -lvs ${gen.get_calibre_lvs_options()} -spice ${gen.get_calibre_lvs_spice_node().abspath()} ${CALIBRE_LVS_OPTIONS} ${gen.rule_file.abspath()} 2>&1 | tee ${gen.get_calibre_lvs_logfile_node().abspath()}'
 
-		logfile = self.generator.path.get_bld().make_node(os.path.join(self.generator.path.bld_dir(),self.env.BRICK_LOGFILES,'calibre_lvs_'+self.generator.cellname+'.log'))
+	def check_output(self,ret,out):
+		for num,line in enumerate(out.split('\n')):
+			if line.find('ERROR:') == 0 or line.find('Error:') == 0:
+				Logs.error("Error in line %d: %s" % (num,line[6:]))
+				ret = 1
+			if line.find('LVS completed. INCORRECT.') == 0:
+				Logs.error("Error in line %d: %s" % (num,line))
+				ret = 1
 
-		run_str = '%s -lvs %s -spice %s %s %s > %s 2>&1' % (self.env.CALIBRE_LVS, conditional_options, self.generator.svdb.make_node(self.generator.cellname+'.sp').abspath()," ".join(self.env.CALIBRE_LVS_OPTIONS), self.generator.rule_file.abspath(),logfile.abspath())
-		out = ""
-		try:
-			out = self.generator.bld.cmd_and_log(run_str, quiet=Context.STDOUT)
-		except Exception as e:
-			out = e.stderr
+		with open(self.generator.get_calibre_lvs_logfile_node().abspath(),'w') as f:
+			f.write(out)
 
-		found_error = 0
-		with open(logfile.abspath(),'r') as lgf:
-			for line in lgf:
-				if re.match('LVS completed. INCORRECT',line):
-					print line
-					found_error = 1
-				elif re.match('ERROR:',line):
-					print line
-					found_error = 1
-				#elif re.match('@W: CL218',line):
-				#	print line
-				#	found_error = 1
+		return ret
 
-		return found_error
-
-		return 0
 
 @TaskGen.feature('calibre_rve_lvs')
 def create_calibre_rve_lvs_task(self):
